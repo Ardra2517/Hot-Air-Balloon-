22ba54ad30c3322c5e9149270f18627aaa4bced2
Ardra2517 <74819734+Ardra2517@users.noreply.github.com>
2021-04-28 20:28:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,32 @@
 var database;
 var position;
 var balloon;
+var balloonImage1,balloonImage2,balloonImage3,balloonImage4;
 
 function preload() {
-
+backgroundImg=loadImage("Hot Air Ballon-01.png");
+balloonImage2=loadImage("Hot Air Ballon-02.png");
+balloonImage3=loadImage("Hot Air Ballon-02.png");
+balloonImage4=loadImage("Hot Air Ballon-04.png");
 }
 
 function setup() {
-  createCanvas(800,400);
+  createCanvas(800,500);
   database=firebase.database();
  
   balloon=createSprite(200,200,40,50);
+  balloon.addImage("img",balloonImage4);
+  balloon.scale=0.5
   
+  var balloonPosition=database.ref('balloon/height');
+  balloonPosition.on("value",readHeight,showError)
+
 }
 
 function draw() {
-  background(255,255,255); 
-  
+  background(backgroundImg); 
+  //image(backgroundImg,200,200,50,50);
+
   if(keyDown(LEFT_ARROW)){
     balloon.x = balloon.x -10;
   }
@@ -31,4 +41,21 @@ function draw() {
   }
 
   drawSprites();
+}
+
+function updateHeight(x,y){
+  database.ref('balloon/height').set({
+      'x':height.x + x,
+      'y': height.y + y
+  })
+}
+
+function readHeight(data){
+  height = data.val();
+  balloon.x = height.x;
+  balloon.y = height.y;
+}
+
+function showError(){
+ console.log("Error in writing to the database");
 }
